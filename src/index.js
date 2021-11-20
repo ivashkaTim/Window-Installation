aba3f942753a0b6b4aa47bdd2ed8ea43774d50a2
ivashkaTim <mr.timofeev1995@mail.ru>
2021-11-20 19:29:11 +0300

    added smooth scroll

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import sliderServices from './modules/sliderServices';
 import modalCall from './modules/modalCall';
 import servicesModal from './modules/servicesModal';
 import documents from './modules/documents';
+import smoothScroll from './modules/smoothScroll';
 
 timer('25 november 2021');
 calculator();
@@ -13,3 +14,4 @@ sliderServices();
 modalCall();
 servicesModal();
 documents();
+smoothScroll();
---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,25 @@
+const smoothScroll = () => {
+  const body = document.querySelector('body');
+  const scrollBtn = document.querySelector('.smooth-scroll');
+  const blockBenefits = document.getElementById('benefits');
+
+  const elemRect = blockBenefits.getBoundingClientRect();
+  scrollBtn.style.opacity = '0';
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY > elemRect.y) {
+      scrollBtn.style.opacity = '1';
+    } else {
+      scrollBtn.style.opacity = '0';
+    }
+  });
+
+  scrollBtn.addEventListener('click', () => {
+    body.scrollIntoView({
+      block: 'center',
+      behavior: 'smooth',
+    });
+  });
+};
+
+export default smoothScroll;
